222a568f44ac2652921df014d9cefa44b490f425
Lucas Oliveira <111022150+Dylucas07@users.noreply.github.com>
2023-08-28 16:54:40 -0300

    UP

---
+++ pop-up/js/editar.js
@@ -0,0 +1,16 @@
+      bioEp.init({
+            html: '<div id="bio_ep_content">' +
+            
+            '<span></span>' +
+            '<span></span>' +
+            '<span></span>' +
+
+            '</div>',
+            css: '#bio_ep {width: 800px; height: 800px; color: #333; background-color: #fafafa; text-align: center;background-image: url(pop-up/imagens-pop/sorteio04.png);background-size: cover;}' +
+            '#bio_ep_content {padding: 255px 0 0 0; font-family: inherit;}' +
+            '#bio_ep_content span:nth-child(2) {display: block; color: #f21b1b; font-size: 32px; font-weight: 600;}' +
+            '#bio_ep_content span:nth-child(3) {display: block; font-size: 16px;}' +
+            '#bio_ep_content span:nth-child(4) {display: block; margin: -5px 0 0 0; font-size: 16px; font-weight: 600;}' +
+            '.bio_btn {display: inline-block; margin: 400px 0 0 0; margin-left: 205px; padding: 8px 10px 10px px; color: #fff; font-size: 15px; font-weight: 900;background-color: #272727; border: 1px solid #FF7E20; cursor: pointer; -webkit-appearance: none; -moz-appearance: none; border-radius: 100px; text-decoration: none;}',
+            cookieExp: 0
+});
